a36e0cd033a2a0e021f68542151409bb87792f5d
haroon10725 <haroongondal347@gmail.com>
2023-04-18 22:42:12 +0500

    Updated the main file.

---
+++ main.py
@@ -0,0 +1,46 @@
+import random
+import colorgram
+import turtle as turtle_module
+turtle_module.colormode(255)
+
+timmy = turtle_module.Turtle()
+timmy.speed("fastest")
+timmy.penup()
+timmy.hideturtle()
+
+
+rgb_colors = []
+colors = colorgram.extract("image.jpg", 30)
+
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    rgb_colors.append(new_color)
+
+
+print(rgb_colors)
+
+color_list = [(202, 164, 110), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153), (168, 99, 102)]
+
+timmy.setheading(225)
+timmy.forward(300)
+timmy.setheading(0)
+
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
+    timmy.dot(20, random.choice(color_list))
+    timmy.forward(50)
+
+    if dot_count % 10 == 0:
+        timmy.setheading(90)
+        timmy.forward(50)
+        timmy.setheading(180)
+        timmy.forward(500)
+        timmy.setheading(0)
+
+
+screen = turtle_module.Screen()
+screen.exitonclick()
